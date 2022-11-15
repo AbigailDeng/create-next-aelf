80e72a2e71aa6b8a352036d618aed85b80e3d518
AbigailDeng <Abigail.deng@ienyan.com>
2022-11-15 14:49:49 +0800

    feat: init

---
+++ bin/create-next-app.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+const program = require("commander");
+const pkg = require("../package.json");
+const chalk = require("chalk");
+const lib = require("..");
+
+const consoles = lib.consoles;
+const createNextApp = lib.createNextApp;
+
+let projectName;
+
+program
+  .version(pkg.version)
+  .arguments("<project-directory>")
+  .usage(`${chalk.green("<project-directory>")} [options]`)
+  .action(function (name) {
+    projectName = name;
+  })
+  .allowUnknownOption()
+  .on("--help", consoles.help)
+  .parse(process.argv);
+
+createNextApp({
+  projectName,
+});
